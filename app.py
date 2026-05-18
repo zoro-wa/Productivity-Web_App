c3bde8933d54cbec851cdb7b670535aed29a5f7c
Bidhan Raj Shakya <shakyaanmol33@gmail.com>
2026-05-18 22:55:57 +0545

    Create database tables

--- app.py
+++ app.py
@@ -23,3 +23,17 @@ def get_db():
         g.db = sqlite3.connect(DATABASE)
         g.db.row_factory =sqlite3.Row # access columns by name
     return g.db
+
+@app.teardown_appcontext        # runs automatically after  every request.
+def close_db(error):            # catch any error that occurs during request handling
+    db = g.pop("db", None)      # remove the db connection from g, if it exists
+    if db is not None:          # if a db connection was established, then only close.
+        db.close()              # close the db connection.
+
+
+@app.route("/")
+def index():
+    db = get_db() #get database connection
+    tasks = db.execute("SELECT * FROM tasks WHERE user_id = ?", 
+                      (session["user_id"],)).fetchall()
+    return render_template("index.html", tasks=tasks) 
